9afb77d666a2a9ec5165a7f00257dc837e27f916
mabulgu <abulgu@redhat.com>
2022-05-27 17:20:23 +0300

    change namespace

--- create-topics.sh
+++ create-topics.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-kfk topics --create --partitions 4 --replication-factor 1 --topic orders -c my-cluster -n reactive-coffeeshop-demo
-kfk topics --create --partitions 4 --replication-factor 1 --topic queue -c my-cluster -n reactive-coffeeshop-demo
+kfk topics --create --partitions 4 --replication-factor 1 --topic orders -c my-cluster -n coffee-shop
+kfk topics --create --partitions 4 --replication-factor 1 --topic queue -c my-cluster -n coffee-shop
 kfk topics --list -c my-cluster -n kafka
+
+
+kfk topics --delete --topic orders -c my-cluster -n reactive-coffeeshop-demo
